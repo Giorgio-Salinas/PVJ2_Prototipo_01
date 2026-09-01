8758addbb33f967a76c5bdc7aa12dc54ef2874da
Giorgio <giorgiosalinas88@gmail.com>
2026-08-31 23:37:40 -0300

    se agregan animaciones al enemigo segun direccion
    se agregan animaciones al enemigo segun direccion

--- enemigo.lua
+++ enemigo.lua
@@ -1,3 +1,5 @@
+local Animacion = require "animacion"
+
 -- Clase Enemigo
 Enemigo = {}
 Enemigo.__index = Enemigo
@@ -8,39 +10,68 @@ function Enemigo:Nuevo(x, y, vel)
     o.x = x
     o.y = y
     o.velocidad = vel or 35
-    o.sprite = love.graphics.newImage("img/Robot_Walk.png")
 
     o.ancho = 16
     o.alto = 16
     o.origen_x = o.ancho / 2
     o.origen_y = o.alto / 2
 
-    -- Frame inicial del robot
-    o.cuadro = love.graphics.newQuad(0, 0, o.ancho, o.alto, o.sprite:getDimensions())
+    -- Animaciones verticales por columna
+    o.animaciones = {
+        abajo     = Animacion.crear("img/Robot_Walk.png", 3, o.ancho, o.alto, 6, true, 0),
+        arriba    = Animacion.crear("img/Robot_Walk.png", 3, o.ancho, o.alto, 6, true, 1),
+        izquierda = Animacion.crear("img/Robot_Walk.png", 3, o.ancho, o.alto, 6, true, 2),
+        derecha   = Animacion.crear("img/Robot_Walk.png", 3, o.ancho, o.alto, 6, true, 3)
+    }
+
+    o.animacionActual = o.animaciones.abajo
 
     return o
 end
 
 function Enemigo:Actualizar(dt, target)
+    local seMueve = false
+
     if target then
-        -- Vector hacia el objetivo
         local dx = target.x - self.x
         local dy = target.y - self.y
         local distancia = math.sqrt(dx * dx + dy * dy)
 
-        -- Si no esta superpuesto, se mueve en direccion al jugador
         if distancia > 1 then
             local dirX = dx / distancia
             local dirY = dy / distancia
 
             self.x = self.x + (dirX * self.velocidad * dt)
             self.y = self.y + (dirY * self.velocidad * dt)
+            seMueve = true
+
+            
+            if math.abs(dx) > math.abs(dy) then
+                if dx > 0 then
+                    self.animacionActual = self.animaciones.derecha
+                else
+                    self.animacionActual = self.animaciones.izquierda
+                end
+            else
+                if dy > 0 then
+                    self.animacionActual = self.animaciones.abajo
+                else
+                    self.animacionActual = self.animaciones.arriba
+                end
+            end
         end
+    end
+
+    self.animacionActual.activado = seMueve
+    if seMueve then
+        Animacion.actualizar(self.animacionActual, dt)
+    else
+        self.animacionActual.indice = 1
     end
 end
 
 function Enemigo:Dibujar()
-    love.graphics.draw(self.sprite, self.cuadro, redondear(self.x), redondear(self.y), 0, 1, 1, self.origen_x, self.origen_y)
+    Animacion.dibujar(self.animacionActual, self.x, self.y, self.origen_x, self.origen_y)
 end
 
 return Enemigo
